e5f984d11ab1a61c081c2977a26f0664f66b4b5f
longquang2612 <longquang2612@gmail.com>
2019-04-03 11:42:37 +0700

    l

---
+++ Tổng quan PHP/Ứng dụng Calculator/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Page Title</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="main.css">
+    <script src="main.js"></script>
+</head>
+<body>
+<form method='post' action="maytinh.php">
+    <input type='text' name='value1'></br>
+
+    <select name='action'></br>
+        <option>Addition</option>
+        <option>Multiplication</option>
+        <option>Subtraction</option>
+        <option>Division</option>
+    </select></br>
+
+    <input type='text' name='value2'></br>
+    <input type="submit" name="submit" value='Calculate Now'>
+    </form>
+    
+</body>
+</html>
