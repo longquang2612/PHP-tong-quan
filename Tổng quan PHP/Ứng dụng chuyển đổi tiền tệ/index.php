820c46670fcce3c5bac04eb36d3512f9774eda18
longquang2612 <longquang2612@gmail.com>
2019-04-07 16:47:35 +0700

    add

--- Tổng quan PHP/Ứng dụng chuyển đổi tiền tệ/index.php
+++ Tổng quan PHP/Ứng dụng chuyển đổi tiền tệ/index.php
@@ -1,14 +1,48 @@
+
 <!DOCTYPE html>
 <html>
 <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Ứng dụng chuyển đổi tiền tệ</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" type="text/css" media="screen" href="main.css">
-    <script src="main.js"></script>
+    <title> Caculator </title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
 </head>
 <body>
+<form method="POST" action= "display.php">
+<div>
+<d1>Amount</d1>
+		<input type="text" name= "text">
+
+<d2>From Currency</d2>
+	<select name="select1">
+  	<option>USD</option>
+  	<option>VND</option>
+	</select>
+
+<d3>To Currency</d3>	
+	<select name="select2">
+  	<option>USD</option>
+  	<option>VND</option>	
+	</select>	
+     
+        <input type="submit" name="submit" value = "Convert" onclick="Change()"/>
+<!-- <script>
+	function Change() {
+	if (document.getElementById("a").value == "USD" &&  document.getElementById("b").value == "VND"){
+	ketqua.value=+eval(ketqua.value)*24000;
+	}
+	else if (document.getElementById("a").value == "VND" && document.getElementById("b").value == "USD"){
+	ketqua.value=+eval(ketqua.value)/24000;
+	}
+	else 
+	ketqua.value=+eval(ketqua.value);
+	}
+</script> -->
+
+</div>
+</form>
+</body>
+</html>
 
     
 </body>
